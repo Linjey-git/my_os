93fd3ad2f0102256e8f446a8c5479e7c6f14f932
_se.ho <linjey.ubi@gmail.com>
2025-03-31 20:56:45 +0100

    Testing part 2

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,11 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(my_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
-mod serial;
-
 use core::panic::PanicInfo;
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-
-#[cfg(not(test))]
-#[panic_handler] // This function is called on panic.
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
-#[panic_handler] // This function is called on panic.
-fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[Failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
-
-// static HELLO: &[u8] = b"Hello World!";
+use my_os::println;
 
 #[unsafe(no_mangle)] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
@@ -56,19 +19,27 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
 #[cfg(test)]
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
-
-    exit_qemu(QemuExitCode::Success);
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    my_os::test_panic_handler(info)
 }
 
-#[test_case]
-fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
-    assert_eq!(0, 1);
-    serial_println!("[ok]");
-}
+// fn test_runner(tests: &[&dyn Fn()]) {
+//     unimplemented!();
+// }
+//
+// #[test_case]
+// fn trivial_assertion() {
+//     // serial_print!("trivial assertion... ");
+//     assert_eq!(1, 1);
+//     // serial_println!("[ok]");
+// }
